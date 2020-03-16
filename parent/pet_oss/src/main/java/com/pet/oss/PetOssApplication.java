9b191854e9595da313c5f7dde74ac4e2ab8a9e8e
ningxiaoming <1214241591@qq.com>
2020-03-16 15:52:36 +0800

    一些简单业务代码

---
+++ parent/pet_oss/src/main/java/com/pet/oss/PetOssApplication.java
@@ -0,0 +1,13 @@
+package com.pet.oss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetOssApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetOssApplication.class, args);
+    }
+
+}
