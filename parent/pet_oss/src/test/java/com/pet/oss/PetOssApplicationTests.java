d8e9c14fc3bff805dd3d941e2bd8cee089165ff1
ningxiaoming <1214241591@qq.com>
2020-03-17 10:47:48 +0800

    一些简单业务代码

---
+++ parent/pet_oss/src/test/java/com/pet/oss/PetOssApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pet.oss;
+
+//import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetOssApplicationTests {
+
+//    @Test
+    void contextLoads() {
+    }
+
+}
